54f4c8bb39347eda19052ead3c34ae6b63f12f99
Clive <clivester900@hotmail.com>
2023-12-19 23:08:15 +0000

    Smelting rework

--- src/io/xeros/content/skills/smithing/Smelting.java
+++ src/io/xeros/content/skills/smithing/Smelting.java
@@ -1,30 +1,31 @@
 package io.xeros.content.skills.smithing;
 
-import java.util.Objects;
-
 import io.xeros.Server;
 import io.xeros.content.achievement.AchievementType;
 import io.xeros.content.achievement.Achievements;
 import io.xeros.model.cycleevent.CycleEventHandler;
 import io.xeros.model.cycleevent.Event;
 import io.xeros.model.entity.player.Player;
+import io.xeros.model.items.GameItem;
 import io.xeros.model.items.ItemAssistant;
-import io.xeros.util.Misc;
+
+import java.util.Objects;
 
 public class Smelting {
 
 	public enum Bars {
-		BRONZE(436, 438, 2349, 1, 6, 2405, true, "bronze"), 
-		IRON(440, -1, 2351, 15, 13, 2406, false, "iron"), 
-		SILVER(442, -1, 2355, 20, 14, 2407, false, "silver"), 
-		STEEL(440, 453, 2353, 30, 18, 2409, true, "steel"), 
-		GOLD(444, -1, 2357, 40, 23, 2410, false, "gold"), 
-		MITHRIL(447, 453, 2359, 50, 30, 2411, true, "mithril"), 
-		ADAMANT(449, 453, 2361, 70, 38, 2412, true, "adamant"), 
-		RUNE(451, 453, 2363, 85, 50, 2413, true, "rune");
+		BRONZE(new GameItem(436), new GameItem(438), 2349, 1, 6, 2405, true, "bronze"),
+		IRON(new GameItem(440), new GameItem(-1), 2351, 15, 13, 2406, false, "iron"),
+		SILVER(new GameItem(442, 1), new GameItem(-1), 2355, 20, 14, 2407, false, "silver"),
+		STEEL(new GameItem(440, 1), new GameItem(453, 1), 2353, 30, 18, 2409, true, "steel"),
+		GOLD(new GameItem(444, 1), new GameItem(-1), 2357, 40, 23, 2410, false, "gold"),
+		MITHRIL(new GameItem(447, 1), new GameItem(453, 4), 2359, 50, 30, 2411, true, "mithril"),
+		ADAMANT(new GameItem(449, 1), new GameItem(453, 6), 2361, 70, 38, 2412, true, "adamant"),
+		RUNE(new GameItem(451, 1), new GameItem(453, 8), 2363, 85, 50, 2413, true, "rune");
 
-		private final int ore1;
-		private final int ore2;
+
+		private final GameItem mainIngredientOre;
+		private final GameItem combinationOre;
 		private final int bar;
 		private final int req;
 		private final int exp;
@@ -32,12 +33,12 @@ public class Smelting {
 		private final boolean twoOres;
 		private final String type;
 
-		int getOre1() {
-			return ore1;
+		GameItem getMainIngredientOre() {
+			return mainIngredientOre;
 		}
 
-		int getOre2() {
-			return ore2;
+		GameItem getCombinationOre() {
+			return combinationOre;
 		}
 
 		public int getBar() {
@@ -64,9 +65,9 @@ public class Smelting {
 			return type;
 		}
 
-		Bars(int ore, int ore2, int bar, int req, int xp, int frame, boolean two, String type) {
-			this.ore1 = ore;
-			this.ore2 = ore2;
+		Bars(GameItem ore, GameItem ore2, int bar, int req, int xp, int frame, boolean two, String type) {
+			this.mainIngredientOre = ore;
+			this.combinationOre = ore2;
 			this.bar = bar;
 			this.req = req;
 			this.exp = xp;
@@ -90,13 +91,13 @@ public class Smelting {
 		c.barType = type;
 		c.smeltAmount = getAmount(amount);
 		c.bar = Bars.forType(c.barType);
-		if(c.bar == null) //Making sure when the "bar" is null it returns
+		if (c.bar == null) //Making sure when the "bar" is null it returns
 			return;
 		boolean hasItems;
 		if (c.bar.twoOres()) {
-			hasItems = hasItems(c, c.bar.getOre1(), c.bar.getOre2(), c.bar.getBar());
+			hasItems = hasItems(c, new GameItem(c.bar.getMainIngredientOre().getId()), new GameItem(c.bar.getCombinationOre().getId()), c.bar.getBar());
 		} else {
-			hasItems = hasItems(c, c.bar.getOre1(), -1, c.bar.getBar());
+			hasItems = hasItems(c, new GameItem(c.bar.getMainIngredientOre().getId()), new GameItem(-1), c.bar.getBar());
 		}
 		if (hasItems) {
 			if (hasReqLvl(c, c.bar.getReq(), c.bar.getBar())) {
@@ -107,10 +108,9 @@ public class Smelting {
 
 	private static void startCycle(final Player c, String usage) {
 		c.getPA().stopSkilling();
-		Server.getEventHandler().submit(new Event<Player>("skilling", c, 1) {
-
+		Server.getEventHandler().submit(new Event<>("skilling", c, 1) {
 			public void execute() {
-				if (attachment == null || attachment.isDisconnected()) {
+				if (c.isDisconnected()) {
 					this.stop();
 					return;
 				}
@@ -126,7 +126,7 @@ public class Smelting {
 			@Override
 			public void stop() {
 				super.stop();
-				if (attachment == null || attachment.isDisconnected()) {
+				if (c.isDisconnected()) {
 					return;
 				}
 				resetSmelting(c);
@@ -143,32 +143,22 @@ public class Smelting {
 		if (c.smeltAmount > 0) {
 			boolean hasItems;
 			if (c.bar.twoOres())
-				hasItems = hasItems(c, c.bar.getOre1(), c.bar.getOre2(), c.bar.getBar());
+				hasItems = hasItems(c, c.bar.getMainIngredientOre(), c.bar.getCombinationOre(), c.bar.getBar());
 			else
-				hasItems = hasItems(c, c.bar.getOre1(), -1, c.bar.getBar());
+				hasItems = hasItems(c, c.bar.getMainIngredientOre(), new GameItem(-1), c.bar.getBar());
 			if (hasItems) {
 				Achievements.increase(c, AchievementType.SMITH, 1);
 				c.getPA().sendSound(2725);
-				if (c.bar.twoOres()) {
-					if (!Objects.equals(usage, "INFERNAL")) {
-						c.startAnimation(899);
-					}
-					c.getItems().deleteItem(c.bar.getOre1(), 1);
-					c.getItems().deleteItem(c.bar.getOre2(), 1);
-					c.getItems().addItem(c.bar.getBar(), 1);
-					c.getPA().addSkillXP((int) (Objects.equals(usage, "INFERNAL") ? c.bar.getExp() / 2 : c.bar.getExp() + (goldSmithGaunts ? percentOfXp : 0)), Player.playerSmithing, true);
-				} else {
-					if (!Objects.equals(usage, "INFERNAL")) {
-						c.startAnimation(899);
-					}
-					c.getItems().deleteItem(c.bar.getOre1(), 1);
-					if ((c.bar.getBar() == 2351 && Misc.random(100) >= 50) && c.playerEquipment[Player.playerRing] != 2568)
-						c.sendMessage("The ore is too impure and you fail to refine it.");
-					else {
-						c.getItems().addItem(c.bar.getBar(), 1);
-						c.getPA().addSkillXP((int) (usage == "INFERNAL" ? c.bar.getExp() / 2 : c.bar.getExp() + (goldSmithGaunts ? percentOfXp : 0)), Player.playerSmithing, true);
-					}
+
+				if (!Objects.equals(usage, "INFERNAL")) {
+					c.startAnimation(899);
 				}
+				c.getItems().deleteItem(c.bar.getMainIngredientOre().getId(), c.bar.getMainIngredientOre().getAmount());
+				if (c.bar.twoOres()) {
+					c.getItems().deleteItem(c.bar.getCombinationOre().getId(), c.bar.getCombinationOre().getAmount());
+				}
+				c.getItems().addItem(c.bar.getBar(), 1);
+				c.getPA().addSkillXP(Objects.equals(usage, "INFERNAL") ? (double) c.bar.getExp() / 2 : c.bar.getExp(), Player.playerSmithing, true);
 			}
 		} else {
 			resetSmelting(c);
@@ -208,22 +198,22 @@ public class Smelting {
 	/**
 	 * Checks if the player has the required items
 	 */
-	private static boolean hasItems(Player c, int firstOre, int secondOre, int createdBar) {
-		String oreOne = ItemAssistant.getItemName(firstOre);
-		String oreTwo = ItemAssistant.getItemName(secondOre);
+	private static boolean hasItems(Player c, GameItem firstOre, GameItem secondOre, int createdBar) {
+		String ingredientOreName = ItemAssistant.getItemName(firstOre.getId());
+		String combinationOreName = ItemAssistant.getItemName(secondOre.getId());
 		String barOutcome = ItemAssistant.getItemName(createdBar);
-		if (secondOre > 0) {
-			if (c.getItems().playerHasItem(firstOre) && c.getItems().playerHasItem(secondOre))
+		if (secondOre.getId() > 0) {
+			if (c.getItems().playerHasItem(firstOre.getId(), firstOre.getAmount()) && c.getItems().playerHasItem(secondOre.getId(), secondOre.getAmount()))
 				return true;
 		} else {
-			if (c.getItems().playerHasItem(firstOre))
+			if (c.getItems().playerHasItem(firstOre.getId(), firstOre.getAmount()))
 				return true;
 		}
-		if (secondOre > 0)
-			c.sendMessage("You need " + oreOne + " and " + oreTwo + " to smelt a " + barOutcome);
+		if (secondOre.getId() > 0)
+			c.sendMessage("You need an " + ingredientOreName + " and " + secondOre.getAmount() + "x " + combinationOreName + " to smelt a " + barOutcome + ".");
 		else
-			c.sendMessage("You need " + oreOne + " to smelt a " + barOutcome);
-			resetSmelting(c);
+			c.sendMessage("You need an " + ingredientOreName + " to smelt a " + barOutcome + ".");
+		resetSmelting(c);
 		return false;
 	}
 
@@ -239,7 +229,7 @@ public class Smelting {
 			return 10;
 		if (Objects.equals(amount, "ALL"))
 			return 28;
-		
+
 		return -1;
 	}
 
